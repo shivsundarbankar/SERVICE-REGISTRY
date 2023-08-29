ce89651f6a268f78aa2776b057e74ac744c279cf
shivsundarbankar <shivsundarbankar@gmail.com>
2023-08-29 23:56:49 +0530

    Initial commit

---
+++ src/test/java/com/shivsundar/sr/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shivsundar.sr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
